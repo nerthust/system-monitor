745b599e7a4d029ca2421582927fd9dd2e2572ec
sebashack <sebastian0092@gmail.com>
2022-09-14 20:46:32 -0500

    Implement utility to compute cpu usage

--- src/core/process.rs
+++ src/core/process.rs
@@ -85,3 +85,64 @@ fn get_proc_cmd_and_name(proc: &Process, stat: &Stat) -> (String, String) {
 
     return (command, name);
 }
+
+fn cpu_usage_calculation(
+    prev_idle: &mut f64,
+    prev_non_idle: &mut f64,
+) -> Result<(f64, f64), RTopError> {
+    use std::io::prelude::*;
+    use std::io::BufReader;
+
+    let mut reader = BufReader::new(std::fs::File::open("/proc/stat")?);
+    let mut first_line = String::new();
+    reader.read_line(&mut first_line)?;
+
+    let (idle, non_idle) = compute_idle_and_non_idle_values(first_line);
+
+    let total = idle + non_idle;
+    let prev_total = *prev_idle + *prev_non_idle;
+
+    let total_delta: f64 = total - prev_total;
+    let idle_delta: f64 = idle - *prev_idle;
+
+    *prev_idle = idle;
+    *prev_non_idle = non_idle;
+
+    let result = if total_delta - idle_delta != 0_f64 {
+        total_delta - idle_delta
+    } else {
+        1_f64
+    };
+
+    let cpu_percentage = if total_delta != 0_f64 {
+        result / total_delta
+    } else {
+        0_f64
+    };
+
+    Ok((result, cpu_percentage))
+}
+
+fn compute_idle_and_non_idle_values(line: String) -> (f64, f64) {
+    fn str_to_f64(val: Option<&str>) -> f64 {
+        val.and_then(|v| v.trim().parse::<f64>().ok())
+            .unwrap_or(0_f64)
+    }
+
+    let mut val = line.split_whitespace();
+    let prefix = val.next().map(|s| s.trim());
+    assert!(prefix == Some("cpu"));
+    let userm = str_to_f64(val.next());
+    let nice: f64 = str_to_f64(val.next());
+    let kernelm: f64 = str_to_f64(val.next());
+    let idle: f64 = str_to_f64(val.next());
+    let iowait: f64 = str_to_f64(val.next());
+    let irq: f64 = str_to_f64(val.next());
+    let softirq: f64 = str_to_f64(val.next());
+    let steal: f64 = str_to_f64(val.next());
+
+    let idle = idle + iowait;
+    let non_idle = userm + nice + kernelm + irq + softirq + steal;
+
+    (idle, non_idle)
+}
