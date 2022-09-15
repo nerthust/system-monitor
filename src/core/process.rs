57856a632ac0137d5a1fff51a52b69e61a19ea7d
sebashack <sebastian0092@gmail.com>
2022-09-14 21:16:30 -0500

    Implement utility to extract cpu usage data for each process

--- src/core/process.rs
+++ src/core/process.rs
@@ -51,8 +51,97 @@ pub struct ProcData {
     pub priority: Pri,
 }
 
+impl ProcData {
+    fn new(
+        proc: Process,
+        stat: Stat,
+        cpu_usage: f64,
+        cpu_fraction: f64,
+        prev_cpu_time: u64,
+        use_current_cpu_total: bool,
+    ) -> (Self, u64) {
+        let (command, name) = get_proc_cmd_and_name(&proc, &stat);
+        let (cpu_usage_percent, new_process_time) = get_cpu_usage(
+            &stat,
+            cpu_usage,
+            cpu_fraction,
+            prev_cpu_time,
+            use_current_cpu_total,
+        );
+        let data = ProcData {
+            pid: proc.pid,
+            parent_pid: stat.ppid,
+            cpu_usage_percent,
+            mem_usage_percent: 0.0,
+            priority: 0,
+            total_disk_read_bytes: 0,
+            total_disk_write_bytes: 0,
+            total_net_received_bytes: 0,
+            total_net_sent_bytes: 0,
+            name: name,
+            command: command,
+            state: (ProcessStatus::from(stat.state).to_string(), stat.state),
+            uid: 0,
+        };
 
-// Helpers
+        (data, new_process_time)
+    }
+}
+
+pub fn read_process_data(
+    prev_idle: &mut f64,
+    prev_non_idle: &mut f64,
+    cpu_times: &mut HashMap<Pid, u64>,
+    use_current_cpu_total: bool,
+) -> Result<Vec<ProcData>, RTopError> {
+    let mut current_pids = HashSet::new();
+    if let Ok((cpu_usage, cpu_percentage)) = cpu_usage_calculation(prev_idle, prev_non_idle) {
+        let data = std::fs::read_dir("/proc")?
+            .filter_map(|dir| {
+                if let Ok(dir) = dir {
+                    if let Ok(pid) = dir.file_name().to_string_lossy().trim().parse::<Pid>() {
+                        if let Ok(proc) = Process::new(pid) {
+                            if let Ok(stat) = proc.stat() {
+                                let prev_proc_cpu_time = *cpu_times.get(&pid).unwrap_or(&0);
+                                let (data, new_proc_cpu_time) = ProcData::new(
+                                    proc,
+                                    stat,
+                                    cpu_usage,
+                                    cpu_percentage,
+                                    prev_proc_cpu_time,
+                                    use_current_cpu_total,
+                                );
+                                cpu_times.insert(pid, new_proc_cpu_time);
+                                current_pids.insert(pid);
+                                return Some(data);
+                            } else {
+                                return None;
+                            }
+                        } else {
+                            return None;
+                        }
+                    }
+
+                    return None;
+                }
+
+                None
+            })
+            .collect();
+
+        let all_pids: HashSet<Pid> = cpu_times.keys().map(|k| *k).collect();
+        all_pids.difference(&current_pids).for_each(|k| {
+            cpu_times.remove(&k);
+        });
+
+        Ok(data)
+    } else {
+        Err(RTopError {
+            err_msg: "Could not compute CPU usage.".to_string(),
+        })
+    }
+}
+
 const MAX_STAT_NAME_LEN: usize = 15;
 
 fn get_proc_cmd_and_name(proc: &Process, stat: &Stat) -> (String, String) {
@@ -108,19 +197,19 @@ fn cpu_usage_calculation(
     *prev_idle = idle;
     *prev_non_idle = non_idle;
 
-    let result = if total_delta - idle_delta != 0_f64 {
+    let active_time = if total_delta - idle_delta != 0_f64 {
         total_delta - idle_delta
     } else {
         1_f64
     };
 
     let cpu_percentage = if total_delta != 0_f64 {
-        result / total_delta
+        active_time / total_delta
     } else {
         0_f64
     };
 
-    Ok((result, cpu_percentage))
+    Ok((active_time, cpu_percentage))
 }
 
 fn compute_idle_and_non_idle_values(line: String) -> (f64, f64) {
@@ -145,4 +234,23 @@ fn compute_idle_and_non_idle_values(line: String) -> (f64, f64) {
     let non_idle = userm + nice + kernelm + irq + softirq + steal;
 
     (idle, non_idle)
+}
+
+fn get_cpu_usage(
+    stat: &Stat,
+    cpu_usage: f64,
+    cpu_percentage: f64,
+    prev_proc_time: u64,
+    use_current_cpu_total: bool,
+) -> (f64, u64) {
+    let new_proc_time = stat.utime + stat.stime;
+    let diff = (new_proc_time - prev_proc_time) as f64;
+
+    if cpu_usage == 0.0 {
+        (0.0, new_proc_time)
+    } else if use_current_cpu_total {
+        ((diff / cpu_usage) * 100_f64, new_proc_time)
+    } else {
+        ((diff / cpu_usage) * 100_f64 * cpu_percentage, new_proc_time)
+    }
 }
