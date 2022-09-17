4e5182fe4428c87c19423b92fc22041fcf4d5763
Danilo Toro <danilotoro492@gmail.com>
2022-09-17 14:36:01 -0500

    implementation command line argument -u

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
+use std::env;
+
 use rtop::core::system_reader::SystemReader;
 use rtop::ui::layout::start_ui;
-fn main() {
-    let sys_data = SystemReader::new(false);
 
+fn check_args(args: Vec<String>) -> Result<bool, &'static str> {
+    if args.len() == 3 {
+        if args[2].eq("-u") {
+            return Ok(true);
+        } else {
+            return Err("Not a valid argument");
+        }
+    }else if args.len() > 3 {
+        return Err("Too many arguments");
+    }
+
+    Ok(false)
+}
+
+fn main() -> Result<(), &'static str>{
+    let args: Vec<String> = env::args().collect();
+    let use_current_cpu_total = check_args(args)?;
+
+    let sys_data = SystemReader::new(use_current_cpu_total);
     let _ui = start_ui(sys_data);
+
+    Ok(())
 }
